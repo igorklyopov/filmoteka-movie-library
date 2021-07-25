82c28216f9a938a5054b06f53d97993ef31ae8b9
Mariia Ros <rosm0099@gmail.com>
2021-07-25 23:50:44 +0200

    added preliminary modal window

---
+++ src/js/modal.js
@@ -0,0 +1,18 @@
+console.log("modal.js start");
+
+const ref5 = {
+	openModalBtn: document.querySelector('[data-action="open-modal"]'),
+	closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+	//backdrop: document.querySelector('.js-backdrop'),
+};
+
+ref5.openModalBtn.addEventListener('click',onOpenModal);
+ref5.closeModalBtn.addEventListener('click',onCloseModal);
+
+function onOpenModal(event) {
+	document.body.classList.add('show-modal');
+}
+
+function onCloseModal(event) {
+	document.body.classList.remove('show-modal');
+}
