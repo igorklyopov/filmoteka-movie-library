57fd875a59a41af37a8cfec3fa65c31d4cd604fd
Misha Krasnonos <muzishutka7@gmail.com>
2021-07-25 11:59:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './sass/main.scss';
 import cardLibraryTpl from './templates/library-card-movie';
-
-const ulEl = document.querySelector('.gallery-library');
-ulEl.insertAdjacentHTML('beforeend', cardLibraryTpl());
