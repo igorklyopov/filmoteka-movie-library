3ca7eee4b1c1049980607512d84117d8c59a2d83
YevheniiYadroch <evheniiyadroch@gmail.com>
2021-07-27 18:29:48 +0300

    adds functionality of modal window

--- src/index.js
+++ src/index.js
@@ -4,20 +4,7 @@ import cardLibraryTpl from './templates/library-card-movie';
 import popularFilmsTpl from './templates/popular-films.hbs';
 import searchFilmsTpl from './templates/home-card-movie';
 import { refs } from './js/refs';
+import './js/onSearch'
+import './js/modalCloseAction'
 
-refs.searchMovieForm.addEventListener('submit', onSearch);
-
-const moviesApiService = new MoviesApiService();
-
-function onSearch(e) {
-  e.preventDefault();
-  refs.sectionContainer.innerHTML = '';
-  moviesApiService.query = e.currentTarget.elements.query.value;
-  //   moviesApiService.resetPage();
-  moviesApiService.fetchMoviesBySearch().then(renderResaultsMarkup);
-}
-
-function renderResaultsMarkup(results) {
-  refs.sectionContainer.insertAdjacentHTML('beforeend', searchFilmsTpl(results));
-}
 
