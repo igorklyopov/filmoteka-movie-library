e87ed32487b276b56ec2fb5172f046739c3ad68c
mikheil sapharidze <portofranko36@gmail.com>
2021-07-27 17:51:29 +0200

    theme-switcher
    theme switcher adde

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cardLibraryTpl from './templates/library-card-movie';
 import popularFilmsTpl from './templates/popular-films.hbs';
 import searchFilmsTpl from './templates/home-card-movie';
 import { refs } from './js/refs';
+import themeSwitcher from './js/theme-switcher';
 
 refs.searchMovieForm.addEventListener('submit', onSearch);
 
@@ -25,53 +26,50 @@ function renderResaultsMarkup(results) {
 import { BASE_URL, API_KEY, SEARCH_MOVIE, TRANDING_DAY, TRANDING_WEEK } from './js/fetchConst';
 import popularMoviesNavTpl from './templates/home-hopular-movies-nav.hbs';
 
-onHomePageLoad()
+onHomePageLoad();
 
 function fetchPopularDayMovies() {
-    return fetch(`${BASE_URL}/${TRANDING_DAY}?api_key=${API_KEY}`)
-      .then(response => {
-      return response.json();
-    });
+  return fetch(`${BASE_URL}/${TRANDING_DAY}?api_key=${API_KEY}`).then(response => {
+    return response.json();
+  });
 }
 
-
-  function fetchPopularWeekMovies() {
-    return fetch(`${BASE_URL}/${TRANDING_WEEK}?api_key=${API_KEY}`)
-    .then(response => {
-      return response.json();
-    });
+function fetchPopularWeekMovies() {
+  return fetch(`${BASE_URL}/${TRANDING_WEEK}?api_key=${API_KEY}`).then(response => {
+    return response.json();
+  });
 }
 
 function renderPopularMoviesNav(navTpl) {
-    refs.sectionContainer.insertAdjacentHTML('afterbegin', navTpl);
+  refs.sectionContainer.insertAdjacentHTML('afterbegin', navTpl);
 }
 
 async function onHomePageLoad() {
-    // refs.sectionContainer.innerHTML = '';
-  await renderPopularMoviesNav(popularMoviesNavTpl())
-  const weekBtn = document.querySelector('.week')
-  const dayBtn = document.querySelector('.day')
+  // refs.sectionContainer.innerHTML = '';
+  await renderPopularMoviesNav(popularMoviesNavTpl());
+  const weekBtn = document.querySelector('.week');
+  const dayBtn = document.querySelector('.day');
   weekBtn.addEventListener('click', onWeekBtnClick);
   dayBtn.addEventListener('click', onDayBtnClick);
-  onDayBtnClick()
+  onDayBtnClick();
 }
 
 function renderPopularMoviesCards(movies) {
-    const movieList = popularFilmsTpl(movies);
-    // refs.sectionContainer.innerHTML= movieList;
+  const movieList = popularFilmsTpl(movies);
+  // refs.sectionContainer.innerHTML= movieList;
   refs.moviesList.insertAdjacentHTML('beforeend', movieList);
 }
 
 function onWeekBtnClick() {
-    refs.moviesList.innerHTML = '';
-    fetchPopularWeekMovies()
+  refs.moviesList.innerHTML = '';
+  fetchPopularWeekMovies()
     .then(movie => renderPopularMoviesCards(movie))
     .catch(console.log);
 }
 
 function onDayBtnClick() {
-    refs.moviesList.innerHTML = '';
-    fetchPopularDayMovies()
+  refs.moviesList.innerHTML = '';
+  fetchPopularDayMovies()
     .then(movie => renderPopularMoviesCards(movie))
     .catch(console.log);
-    }
+}
