9b9ffddcdc9359019d2fd9f7c02bc70ad5bfc8f2
Misha Krasnonos <muzishutka7@gmail.com>
2021-07-27 04:37:30 +0300

    add class find by word

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 import './sass/main.scss';
-import { fetchPopularDayMovies, fetchPopularWeekMovies } from './js/apiService';
+import { fetchPopularDayMovies, fetchPopularWeekMovies, MoviesApiService } from './js/apiService';
 import cardLibraryTpl from './templates/library-card-movie';
 import popularFilmsTpl from './templates/popular-films.hbs';
 import { refs } from './js/refs';
 
+const moviesApiService = new MoviesApiService();
+
 document.addEventListener('DOMContentLoaded', onHomePageLoad);
-refs.weekBtn.addEventListener('click', onWeekBtnClick);
+// refs.weekBtn.addEventListener('click', onWeekBtnClick);
+refs.searchMovieForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  moviesApiService.query = e.currentTarget.elements.query.value;
+  //   moviesApiService.resetPage();
+  moviesApiService.fetchMoviesBySearch();
+}
 
 function onHomePageLoad() {
-    refs.sectionContainer.innerHTML = '';
-    fetchPopularDayMovies()
+  refs.sectionContainer.innerHTML = '';
+  fetchPopularDayMovies()
     .then(movie => renderPopularMoviesCards(movie))
     .catch(console.log);
 }
 
 function onWeekBtnClick() {
-    refs.sectionContainer.innerHTML = '';
-    fetchPopularWeekMovies()
+  refs.sectionContainer.innerHTML = '';
+  fetchPopularWeekMovies()
     .then(movie => renderPopularMoviesCards(movie))
     .catch(console.log);
 }
 
 function renderPopularMoviesCards(movies) {
-    const movieList = popularFilmsTpl(movies);
-    refs.sectionContainer.insertAdjacentHTML('afterbegin', movieList);
+  const movieList = popularFilmsTpl(movies);
+  refs.sectionContainer.insertAdjacentHTML('afterbegin', movieList);
 }
-
-
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export const refs = {
   galleryLibraryEl: document.querySelector('.gallery-library'),
+  searchMovieForm: document.querySelector('.search-movie-form'),
   sectionContainer: document.querySelector('.main-section-container'),
   dayBtn: document.querySelector('.day'),
   weekBtn: document.querySelector('.week'),
