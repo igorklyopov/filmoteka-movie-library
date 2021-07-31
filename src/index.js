55c7d21824232c2fa67351a0ea49a7a9fa80036d
Igor Klyopov <shoot8202@gmail.com>
2021-07-31 14:42:52 +0300

    change import MoviesApiService

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import './sass/main.scss';
-import { MoviesApiService } from './js/apiService';
 // import cardLibraryTpl from './templates/library-card-movie';
 // import popularFilmsTpl from './templates/popular-films.hbs';
 // import searchFilmsTpl from './templates/home-card-movie';
 // import modalMovieInfo from './templates/modal-movie-content';
+import MoviesApiService from './js/apiService';
 import { refs } from './js/refs';
 import './js/onSearch'
 // import './js/modalCloseAction'
 import './js/toTopButton'
 // import genres from './js/genres_ids.json'
-import themeSwitcher from './js/theme-switcher';
+// import themeSwitcher from './js/theme-switcher';
 import './js/spa';
-import { onHomePageLoad, renderPopularMoviesCards, onWeekBtnClick, onDayBtnClick, loadMorePopMovies} from './js/popMoviesLoadFunctions';
+import { onHomePageLoad, onWeekBtnClick, onDayBtnClick, loadMorePopMovies} from './js/popMoviesLoadFunctions';
 
 onHomePageLoad()
 
