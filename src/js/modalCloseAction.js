6d53c238aef0271f621d7ddc6186f5e14f6ed3c6
YevheniiYadroch <evheniiyadroch@gmail.com>
2021-08-01 19:38:09 +0300

    adds listener for ESC button to close modal window

--- src/js/modalCloseAction.js
+++ src/js/modalCloseAction.js
@@ -6,7 +6,14 @@ const modalMovieOverlay = document.querySelector('.modal-overlay')
 const clickHandlerClose = function () {
     modal.classList.remove('modal-movie-card-visible');
     modalInfo.innerHTML = '';
+};
+
+const keyupHandlerClose = function (evt) {
+    if (evt.code === 'Escape') {
+        clickHandlerClose();
+    }
 }
 
 clsBtn.addEventListener('click', clickHandlerClose);
 modalMovieOverlay.addEventListener('click', clickHandlerClose);
+window.addEventListener('keyup', keyupHandlerClose);
